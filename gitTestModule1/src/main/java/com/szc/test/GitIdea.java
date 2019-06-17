f0a4aa272b0f3e157f592164cb55067f577cf318
szc <2996979969@qq.com>
2019-06-17 11:29:37 +0800

    int i 66;

--- gitTestModule1/src/main/java/com/szc/test/GitIdea.java
+++ gitTestModule1/src/main/java/com/szc/test/GitIdea.java
@@ -3,6 +3,9 @@ package com.szc.test;
 public class GitIdea {
 
     public static void main(String[] args) {
+
+        int i = 66;
+
         System.out.println("idea上的git");
         System.out.println("做个修改");
     }
